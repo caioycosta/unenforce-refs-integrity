33d5ee7ce93621426be88db33a03c9d1cd1c3975
caioycosta <caiocc12@gmail.com>
2020-07-23 09:48:18 -0300

    Initial version from superuser
    Based on answer https://superuser.com/a/1561283/177447

---
+++ unenforce-refs-integrity.ps1
@@ -0,0 +1,13 @@
+﻿$stack = New-Object System.Collections.Stack
+#Load first level
+Get-ChildItem -Path "YOUR-PATH-HERE" | ForEach-Object { $stack.Push($_) }
+#Recurse
+while($stack.Count -gt 0 -and ($item = $stack.Pop())) {
+    if ($item.PSIsContainer)
+    {
+        Write-Host "Recursing on item $($item.FullName)"
+        Get-ChildItem -Path $item.FullName | ForEach-Object { $stack.Push($_) }
+    } else {
+        Write-Host "Processing item $($item.FullName)"
+    }
+}
