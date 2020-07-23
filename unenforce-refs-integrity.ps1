ad0c1946d3b1ea0e897721941ed5fb9c580c7910
caioycosta <caiocc12@gmail.com>
2020-07-23 12:02:09 -0300

    Update unenforce-refs-integrity.ps1
    Include the starting Path as an object to be Unenforced too
    Also Unenforce directories, not only files

--- unenforce-refs-integrity.ps1
+++ unenforce-refs-integrity.ps1
@@ -1,15 +1,16 @@
-$path = "E:\"
+$path = "YOUR-PATH-HERE"
 $stack = New-Object System.Collections.Stack
 #Load first level
-Get-ChildItem -Force -Path $path | ForEach-Object { $stack.Push($_) }
+$stack.Push($(Get-Item -Force -Path $path))
 #Recurse
 while($stack.Count -gt 0 -and ($item = $stack.Pop())) {
     if ($item.PSIsContainer)
     {
         Write-Host "Recursing on item $($item.FullName)"
+        $item | Set-FileIntegrity -Enable $True -Enforce $False
         Get-ChildItem -Force -Path $item.FullName | ForEach-Object { $stack.Push($_) }
     } else {
         Write-Host "Processing item $($item.FullName)"
-        $item | Set-FileIntegrity -Enable $True -Enforce $False 
+        $item | Set-FileIntegrity -Enable $True -Enforce $False
     }
 }
