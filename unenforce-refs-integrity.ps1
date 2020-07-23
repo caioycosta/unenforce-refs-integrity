ca46085eb2ec054d62369272b0d50b3bcb838254
caioycosta <caiocc12@gmail.com>
2020-07-23 10:04:28 -0300

    Update unenforce-refs-integrity.ps1
    Set Enforced False, Path as a variable, now traverses hidden/system files and directories.

--- unenforce-refs-integrity.ps1
+++ unenforce-refs-integrity.ps1
@@ -1,13 +1,15 @@
-﻿$stack = New-Object System.Collections.Stack
+$path = "E:\"
+$stack = New-Object System.Collections.Stack
 #Load first level
-Get-ChildItem -Path "YOUR-PATH-HERE" | ForEach-Object { $stack.Push($_) }
+Get-ChildItem -Force -Path $path | ForEach-Object { $stack.Push($_) }
 #Recurse
 while($stack.Count -gt 0 -and ($item = $stack.Pop())) {
     if ($item.PSIsContainer)
     {
         Write-Host "Recursing on item $($item.FullName)"
-        Get-ChildItem -Path $item.FullName | ForEach-Object { $stack.Push($_) }
+        Get-ChildItem -Force -Path $item.FullName | ForEach-Object { $stack.Push($_) }
     } else {
         Write-Host "Processing item $($item.FullName)"
+        $item | Set-FileIntegrity -Enable $True -Enforce $False 
     }
 }
